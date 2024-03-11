e26dfb6513101e72e6caee69816b41699cc821e5
Pratap144 <72606594+Pratap144@users.noreply.github.com>
2024-03-11 15:44:59 +0530

    gsap animations added

--- app.js
+++ app.js
@@ -6,3 +6,60 @@ function preload() {
   document.body.classList.remove("noScroll");
   preloader.style.visibility = "hidden";
 }
+
+// let tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.container-new',
+//     start: 'start center',
+//     ease: elastic,
+//     end: 'end center',
+//     scrub: true,
+//     markers: false
+//   }
+// })
+
+// tl.to('.container-new', {
+//   x: 80
+// })
+
+gsap.to('.container-scroll-1', {
+  scale: 0.85,
+  ease: 'none',
+  // scale: 0.81,
+  scrollTrigger: {
+      trigger: '.container-scroll-1',
+      Scroller: 'body',
+      start: 'top 0%',
+      end: 'bottom 50%',
+      // markers: true,
+      scrub: 0.5
+  }
+})
+
+gsap.from('.container-scroll-2', {
+  x: 0,
+  y: 0,
+  opacity: 0,
+  scrollTrigger: {
+      trigger: '.container-scroll-1',
+      Scroller: 'body',
+      start: 'center center',
+      end: 'bottom center',
+      markers: true,
+      scrub: 2
+  }
+})
+
+gsap.from('.container-scroll-3', {
+  x: 0,
+  y: 0,
+  opacity: 0,
+  scrollTrigger: {
+      trigger: '.container-scroll-2',
+      Scroller: 'body',
+      start: 'center center',
+      end: 'bottom center',
+      markers: true,
+      scrub: 2
+  }
+})
