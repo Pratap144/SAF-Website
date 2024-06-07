569d4f3de16256002235db0ba4d816494c50d434
Venkatesh <75016701+PedisettiVenkatesh@users.noreply.github.com>
2024-06-08 01:54:33 +0530

    v0.8.0

--- src/index.js
+++ src/index.js
@@ -91,7 +91,7 @@ document.getElementById("bannerClose").addEventListener("click", (e) => {
 // color pricker script
 
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
+let myButton = document.getElementById("btn-back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -103,14 +103,14 @@ function scrollFunction() {
     document.body.scrollTop > 1000 ||
     document.documentElement.scrollTop > 1000
   ) {
-    mybutton.style.display = "block";
+    myButton.style.display = "block";
     document.getElementById("banner").classList.remove("visually-hidden");
   } else {
-    mybutton.style.display = "none";
+    myButton.style.display = "none";
   }
 }
 // When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
+myButton.addEventListener("click", backToTop);
 
 function backToTop() {
   document.body.scrollTop = 0;
