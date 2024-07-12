a6bd405a90a4f47ef6c00739e77132a4ceb9db76
Venkatesh <75016701+PedisettiVenkatesh@users.noreply.github.com>
2024-07-12 16:26:28 +0530

    FINAL

---
+++ dist/index.ccf8a6ac.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("registrationDate").innerHTML="July 12, 2024"});
+//# sourceMappingURL=index.ccf8a6ac.js.map
---
+++ src/registraionDate.js
@@ -0,0 +1,5 @@
+// Change the content here
+document.addEventListener("DOMContentLoaded", function () {
+  var element = document.getElementById("registrationDate");
+  element.innerHTML = "July 12, 2024";
+});
