ee120d4ffec3f323e857d62b59f02ca59177957a
Venkatesh <75016701+PedisettiVenkatesh@users.noreply.github.com>
2024-07-14 19:04:18 +0530

    parcel update

---
+++ src/static/content.js
@@ -0,0 +1,19 @@
+window.REGISTRATION_DATE =
+  "Month of August Starting From 1st August to 15th August,2024 [Last Date of Registration -31/07/2024]";
+window.MODULE_NUMBERS = "Module 1 - 5";
+window.TENTATIVE_DATE_01 =
+  "For the Month of September: 4th September to 18th September, 2024";
+window.TENTATIVE_DATE_02 =
+  "For the Month of October: 3rd October to 17th October, 2024";
+window.TENTATIVE_DATE_03 =
+  "For the Month of November: 1st November to 15th November, 2024";
+
+window.MODULE_01_DAYS = "06";
+window.MODULE_02_DAYS = "08";
+window.MODULE_03_DAYS = "10";
+window.MODULE_04_DAYS = "15";
+
+window.MODULE_01_PRICE = "6,000";
+window.MODULE_02_PRICE = "8,000";
+window.MODULE_03_PRICE = "12,000";
+window.MODULE_04_PRICE = "22,500";
